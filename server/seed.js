61c87aba888289ae015fe7df6a7f6a18b50f6c80
Ben <bemorrison16@gmail.com>
2024-07-23 11:23:11 -0400

    Added unique constraint on email

--- server/seed.js
+++ server/seed.js
@@ -6,7 +6,7 @@ async function createTables() {
     CREATE TABLE IF NOT EXISTS users(
     id UUID PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     money INTEGER NOT NULL,
     goal INTEGER NOT NULL,
